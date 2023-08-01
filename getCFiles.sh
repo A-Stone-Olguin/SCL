e2e8404d677eec04019662352426b6e0ac6e1aa8
Stone Olguin <olguinstone@gmail.com>
2023-08-01 11:54:35 -0600

    Removed need for c files and make

--- getCFiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Gets all of the c files in an array
-rm -f cfiles.txt
-readarray -d '' c_array < <(find * -name "*.c" -print0 )
-
-for file in ${c_array[@]}; do
-  echo $file >> cfiles.txt
-done
