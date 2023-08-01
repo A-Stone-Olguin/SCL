1e18413a4bfea1e120d2655787208eda2ff13892
Stone Olguin <olguinstone@gmail.com>
2023-08-01 12:17:58 -0600

    Minimal build from chipwhisperer bash script

---
+++ chipwhisperer-minimal/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='chipwhisperer',
+    version='5.6.1',
+    description='ChipWhisperer Side-Channel Analysis Tool',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='NewAE Technology Inc',
+    author_email='sales@newae.com',
+    license='apache',
+    url='https://www.chipwhisperer.com',
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    install_requires=[
+        'pyserial',
+        'libusb1',
+        'Cython',
+    ],
+    project_urls={
+        'Documentation': 'https://chipwhisperer.readthedocs.io',
+        'Source': 'https://github.com/newaetech/chipwhisperer-minimal',
+        'Issue Tracker': 'https://github.com/newaetech/chipwhisperer-minimal/issues',
+    },
+    python_requires='~=3.6',
+)
